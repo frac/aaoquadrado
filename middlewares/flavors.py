86cc9034ff50b8aa0ccffaa88b481411bf150ea3
adriano petrich <petrich@gmail.com>
2009-09-10 20:16:35 -0300

    added name to the title

--- middlewares/flavors.py
+++ middlewares/flavors.py
@@ -2,7 +2,11 @@ from random import choice
 
 def flavor_chooser(request):
     flavors = ["tree", "sea", "red"]
-    flavor  = choice(flavors)
+    if "flavor" in request.GET.keys():
+        flavor =  request.GET["flavor"]
+    else:
+        flavor  = choice(flavors)
+
 
     return {
         'flavor': flavor,
