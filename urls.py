f6d4f7aef7902f2e39766a56c5f3ef69e285f133
adriano petrich <petrich@gmail.com>
2009-10-20 16:38:26 -0200

    better layout

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.views.generic.simple import direct_to_template, redirect_to
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,13 +8,14 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^aaoquadrado/', include('aaoquadrado.foo.urls')),
-    (r'^info/', include('aaoquadrado.info.urls')),
-    (r'^$', direct_to_template, {'template': "index.html"}),
+    #    (r'^info/', include('aaoquadrado.info.urls')),
+    #(r'^$', direct_to_template, {'template': "index.html"}),
+    (r'^$', redirect_to, {'url': "/home"}),
 
-    (r'^detalhes$', direct_to_template, {'template': "detalhes.html"}),
+    #(r'^detalhes$', direct_to_template, {'template': "detalhes.html"}),
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
