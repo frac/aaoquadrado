6cf108ee8627483997bc4cc921b119817791f12c
root <root@hardy2.(none)>
2009-10-20 18:40:38 +0000

    backporting

--- urls.py
+++ urls.py
@@ -8,13 +8,13 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^aaoquadrado/', include('aaoquadrado.foo.urls')),
-    (r'^info/', include('aaoquadrado.info.urls')),
+    #(r'^info/', include('aaoquadrado.info.urls')),
     (r'^$', direct_to_template, {'template': "index.html"}),
 
-    (r'^detalhes$', direct_to_template, {'template': "detalhes.html"}),
+    #(r'^detalhes$', direct_to_template, {'template': "detalhes.html"}),
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
